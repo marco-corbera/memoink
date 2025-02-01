0fb917ce671522c75c9260b9114bce1ba52b0666
Marco Corbera <marco.corbera1@gmail.com>
2025-02-01 02:31:06 -0500

    remove context

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -1,17 +1,13 @@
 "use client"
 
-import { NotesProvider } from "@/context/NotesContext" // Importa el contexto
-
 export default function NotesLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <NotesProvider>
-      <div className="min-h-screen bg-memoink-background flex">
-        <main className="flex-1 p-16">{children}</main>
-      </div>
-    </NotesProvider>
+    <div className="min-h-screen bg-memoink-background flex">
+      <main className="flex-1 p-16">{children}</main>
+    </div>
   )
 }
